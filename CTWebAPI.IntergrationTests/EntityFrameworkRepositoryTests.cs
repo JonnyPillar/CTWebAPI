b578b3c8294ab607874e4f7e734abec464e525e0
livelyjp <jonny@jonnypillar.co.uk>
2014-06-07 20:06:48 +0100

    Started considering intergration tests

---
+++ CTWebAPI.IntergrationTests/EntityFrameworkRepositoryTests.cs
@@ -0,0 +1,30 @@
+﻿using CTWebAPI.Models;
+using CTWebAPI.Repository.DataLayer;
+using NUnit.Framework;
+
+namespace CTWebAPI.IntergrationTests
+{
+    [TestFixture]
+    [Category("Intergration")]
+    public class EntityFrameworkRepositoryTests
+    {
+        private EFUnitOfWork _efUnitOfWork;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _efUnitOfWork = new EFUnitOfWork();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _efUnitOfWork.Dispose();
+        }
+
+        [Test]
+        public void UserRepository_()
+        {
+        }
+    }
+}
